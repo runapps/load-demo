35fdcfa801372c0e8842fd676607f71576a5fd63
skyikho <skyvtex@gmail.com>
2020-09-15 01:58:36 +0900

    graceful shutdown 구현

--- src/main/java/com/acornsoft/cocktaildemo/CocktailDemoApplication.java
+++ src/main/java/com/acornsoft/cocktaildemo/CocktailDemoApplication.java
@@ -2,6 +2,8 @@ package com.acornsoft.cocktaildemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -15,7 +17,14 @@ public class CocktailDemoApplication extends SpringBootServletInitializer {
 
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(CocktailDemoApplication.class, args);
+        //SpringApplication.run(CocktailDemoApplication.class, args);
+
+        SpringApplication application = new SpringApplicationBuilder()
+                .sources(CocktailDemoApplication.class)
+                .listeners(new ApplicationPidFileWriter("./application.pid"))
+                .build();
+
+        application.run(args);
     }
 
 }
---
+++ src/main/java/com/acornsoft/cocktaildemo/demo/controller/LongProcessController.java
@@ -0,0 +1,15 @@
+package com.acornsoft.cocktaildemo.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LongProcessController {
+
+    @RequestMapping("/long-process")
+    public String pause() throws InterruptedException {
+        Thread.sleep(20000);
+        return "Process finished";
+    }
+
+}
